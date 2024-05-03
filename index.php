298d53716f233ee1e326ed201bb9a5eb0df95416
giannisaladino <saladinogianni6@gmail.com>
2024-05-03 15:20:19 +0200

    ADD: form and "server" file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BadWords</title>
+</head>
+<body>
+    <main>
+        <form action="./server.php">
+            <label for="message" id="message">Inserisci un messaggio</label>
+            <textarea name="message" id="message" placeholder="scrivi..." ></textarea>
+            <button>Invia</button>
+        </form>
+    </main>
+</body>
+</html>
